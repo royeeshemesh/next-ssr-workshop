7d8ea63ac3d82898350329e631d0c3b6e509ffa7
Royee Shemesh <royee_shemesh@intuit.com>
2019-05-29 14:04:33 +0300

    SAVEPOINT

--- app-server/server.js
+++ app-server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const proxy = require('http-proxy-middleware');
 
 // create express app
 const app = express();
 
 // set public folder as express static assets
 app.use(express.static('public', {index: false}));
+
+app.use(proxy(['/api'],{
+  target: 'http://localhost:8888/',
+  changeOrigin: true,
+  xfwd: true,
+}));
 
 // listen to root request
 app.get('*', (req, res) => {

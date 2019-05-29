e84147d09d1e6ccd1ff3ddd6c6424f829f73ab92
Royee Shemesh <royee_shemesh@intuit.com>
2019-05-29 14:04:33 +0300

    SAVEPOINT

---
+++ app-server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+// create express app
+const app = express();
+
+// set public folder as express static assets
+app.use(express.static('public', {index: false}));
+
+// listen to root request
+app.get('*', (req, res) => {
+  res.send(`
+<html>
+<body>
+    <div id="root"></div>
+    <script src="/client-bundle.js"></script>
+</body>
+</html>    
+    `);
+});
+
+// start server
+app.listen(3000, () => {
+  console.info('SSR server is listening on port 5505');
+});

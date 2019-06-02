8e7d41b30499db541e061e48921bac75b4711575
Royee Shemesh <royee_shemesh@intuit.com>
2019-06-02 10:59:53 +0300

    SAVEPOINT

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-// import './Comments.css';
 import CommentsList from '../components/CommentsList';
 import SearchBox from '../components/SearchBox';
 
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -4,8 +4,6 @@ import PropTypes from 'prop-types';
 import UsersList from '../components/UsersList';
 import UserDetails from '../components/UserDetails';
 
-// import './Users.css';
-
 class Users extends Component {
   async componentDidMount() {
     if (!this.props.items) {

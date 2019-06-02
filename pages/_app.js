392f8a7f4d1455893a68bf73f13e53b3b5cc71e8
Royee Shemesh <royee_shemesh@intuit.com>
2019-06-02 14:38:51 +0300

    SAVEPOINT

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,40 @@
+import axios from 'axios';
 import React from 'react';
 import App, {Container} from 'next/app';
 import {Provider} from 'react-redux';
 import {initializeStore} from '../src/store';
 import Link from 'next/link';
 
+// isServer for client/server differentiation
+const isServer = typeof window === 'undefined';
+
+let axiosInstance;
+
+const getOrCreateStore = initialState => {
+  // Always make a new store if server, otherwise state is shared between requests
+  if (isServer) {
+    return initializeStore(initialState, axiosInstance);
+  }
+
+  // Create store if unavailable on the client and set it on the window object
+  if (!window['__NEXT_REDUX_STORE__']) {
+    window['__NEXT_REDUX_STORE__'] = initializeStore(initialState, axiosInstance);
+  }
+  return window['__NEXT_REDUX_STORE__']
+};
+
 class MyApp extends App {
 
   static async getInitialProps({ Component, ctx }) {
-    ctx.reduxStore = initializeStore();
+    if (isServer) {
+      axiosInstance = axios.create({
+        baseURL: 'http://127.0.0.1:3000/',
+        withCredentials: true,
+        headers: {...ctx.req.headers }
+      });
+    }
+
+    ctx.reduxStore = getOrCreateStore();
 
     let pageProps = {};
     if (Component.getInitialProps) {
@@ -23,7 +50,14 @@ class MyApp extends App {
   constructor(props) {
     super(props);
 
-    this.reduxStore = initializeStore(props.reduxInitialState);
+    if (!isServer) {
+      axiosInstance = axios.create({
+        baseURL: '/',
+        withCredentials: true,
+      });
+    }
+
+    this.reduxStore = getOrCreateStore(props.reduxInitialState);
   }
 
   render() {
--- src/store/posts/posts-actions.js
+++ src/store/posts/posts-actions.js
@@ -1,17 +1,15 @@
-import axios from 'axios';
-
 export const actionTypes = {
   FETCH_POSTS: 'fetchPosts',
   FETCH_POSTS_SUCCESS: 'fetchPostsSuccess',
   FETCH_POSTS_FAILED: 'fetchPostsFailed',
 };
 
-export const fetchPosts = () => async dispatch => {
+export const fetchPosts = () => async (dispatch, getState, api) => {
   dispatch({
     type: actionTypes.FETCH_POSTS,
   });
 
-  const result = await axios.get('/api/posts');
+  const result = await api.get('/api/posts');
 
   dispatch({
     type: actionTypes.FETCH_POSTS_SUCCESS,

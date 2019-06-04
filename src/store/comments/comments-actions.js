ee9bfa3485497db0de2e16e7f13792d039b0297a
Royee Shemesh <royee_shemesh@intuit.com>
2019-06-04 16:50:15 +0300

    SAVEPOINT

--- src/store/comments/comments-actions.js
+++ src/store/comments/comments-actions.js
@@ -1,15 +1,17 @@
+import axios from 'axios';
+
 export const actionTypes = {
   FETCH_COMMENTS: 'fetchComments',
   FETCH_COMMENTS_SUCCESS: 'fetchCommentsSuccess',
   FETCH_COMMENTS_FAILED: 'fetchCommentsFailed',
 };
 
-export const fetchComments = () => async (dispatch, getState, api) => {
+export const fetchComments = () => async dispatch => {
   dispatch({
     type: actionTypes.FETCH_COMMENTS,
   });
 
-  const result = await api.get('/api/comments');
+  const result = await axios.get('/api/comments');
 
   dispatch({
     type: actionTypes.FETCH_COMMENTS_SUCCESS,
